db8995834a65fbe5400c12b5d4aa2dd1bbb71ee8
bhpuja <vpuja0421@gmail.com>
2025-01-17 09:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+// Add interactivity for debugging or logging clicks on navigation items
+document.querySelectorAll(".navbar li").forEach((item) => {
+    item.addEventListener("click", () => {
+        console.log(`You clicked on: ${item.textContent}`);
+        alert(`You clicked on: ${item.textContent}`);
+    });
+});
